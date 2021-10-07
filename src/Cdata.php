3a64596e746bd993231f7d83c9150f47169a7b63
Josh Bruce <josh@8fold.pro>
2021-10-07 08:06:07 -0500

    Fixes #3
    Found that the implementation of the tests was incorrect format; further demonstrating purpose of library.

---
+++ src/Cdata.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eightfold\XMLBuilder;
+
+// use \Stringable;
+
+// TODO: PHP8 - specify implementing Stringable
+class Cdata // implements Stringable
+{
+    private string $content = '';
+
+    public static function create(string $content): Cdata
+    {
+        return new Cdata($content);
+    }
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function build(): string
+    {
+        return '<![CDATA[' . $this->content() . ']]>';
+    }
+
+    public function __toString(): string
+    {
+        return $this->build();
+    }
+
+    private function content(): string
+    {
+        return $this->content;
+    }
+}
