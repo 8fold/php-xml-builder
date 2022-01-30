483b410111df1f81910effe083b8f9ac6e66c145
Josh Bruce <josh@8fold.pro>
2022-01-30 15:43:50 -0600

    re-engineer: Element baseline tests

--- tests/ElementBaselineTest.php
+++ tests/ElementBaselineTest.php
@@ -1,70 +1,99 @@
 <?php
 
+declare(strict_types=1);
+
+namespace Eightfold\XMLBuilder\Tests;
+
+use PHPUnit\Framework\TestCase;
+
 use Eightfold\XMLBuilder\Element;
 
-test('Element is Stringable', function() {
-    expect(
-        (string) Element::create('root')->props('id 6', 'property hello')
-            ->omitEndTag()
-    )->toBe(
-        '<root id="6" property="hello" />'
-    );
-});
+class ElementBaselingTests extends TestCase
+{
+    /**
+     *@test
+     */
+    public function element_is_stringable(): void
+    {
+        $this->assertEquals(
+            (string) Element::create('root')->props('id 6', 'property hello')
+                ->omitEndTag(),
+            '<root id="6" property="hello" />'
+        );
+    }
 
-test('Element can omit end tag', function() {
-    expect(
-        Element::create('root')->props('id 6', 'property hello')
-            ->omitEndTag()->build()
-    )->toBe(
-        '<root id="6" property="hello" />'
-    );
-});
+    /**
+     *@test
+     */
+    public function element_can_omit_end_tag(): void
+    {
+        $this->assertEquals(
+            Element::create('root')->props('id 6', 'property hello')
+                ->omitEndTag()->build(),
+            '<root id="6" property="hello" />'
+        );
+    }
 
-test('Element can have properties', function() {
-    expect(
-        Element::create('root')->props('id 6', 'property hello')->build()
-    )->toBe(
-        '<root id="6" property="hello"></root>'
-    );
-});
+    /**
+     *@test
+     */
+    public function element_can_have_properties(): void
+    {
+        $this->assertEquals(
+            Element::create('root')->props('id 6', 'property hello')->build(),
+            '<root id="6" property="hello"></root>'
+        );
+    }
 
-test('Element can use shorthand', function() {
-    expect(
-        Element::root(
-            Element::child(
-                Element::grandchild('Xavier')->omitEndTag()
-                    ->props('name Xavier'),
-                '<!CDATA[String]]>'
-            )
-        )->build()
-    )->toBe(
-        '<root><child><grandchild name="Xavier" />Xavier<!CDATA[String]]></child></root>'
-    );
-});
+    /**
+     *@test
+     */
+    public function element_can_use_shorthand(): void
+    {
+        $this->assertEquals(
+            Element::root(
+                Element::child(
+                    Element::grandchild('Xavier')->omitEndTag()
+                        ->props('name Xavier'),
+                    '<!CDATA[String]]>'
+                )
+            )->build(),
+            '<root><child><grandchild name="Xavier" />Xavier<!CDATA[String]]></child></root>'
+        );
+    }
 
-test('Element can accept content', function() {
-    expect(
-        Element::create('root',
-            Element::create('child',
-                Element::create('grandchild'),
-                '<!CDATA[String]]>'
-            )
-        )->build()
-    )->toBe(
-        '<root><child><grandchild></grandchild><!CDATA[String]]></child></root>'
-    );
-});
+    /**
+     *@test
+     */
+    public function element_can_accept_content(): void
+    {
+        $this->assertEquals(
+            Element::create('root',
+                Element::create('child',
+                    Element::create('grandchild'),
+                    '<!CDATA[String]]>'
+                )
+            )->build(),
+            '<root><child><grandchild></grandchild><!CDATA[String]]></child></root>'
+        );
+    }
 
-test('Element can be static initialized', function() {
-    expect(
-        Element::create('tag')->build()
-    )->toBe(
-        '<tag></tag>'
-    );
-});
+    /**
+     *@test
+     */
+    public function element_can_initialized_statically(): void
+    {
+        $this->assertEquals(
+            Element::create('tag')->build(),
+            '<tag></tag>'
+        );
+    }
 
-test('Element exists', function() {
-    $this->assertTrue(
-        class_exists(Element::class)
-    );
-});
+    /**
+     *@test
+     */
+    public function element_exists(): void
+    {
+        $this->assertTrue(class_exists(Element::class));
+    }
+}
