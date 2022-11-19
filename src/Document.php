5c85969a3fdd35dc57c5cc93faf8513c30a49761
Josh Bruce <15252830+joshbruce@users.noreply.github.com>
2022-11-19 14:34:16 -0500

    feature: Users can set version, encoding, and standalone

--- src/Document.php
+++ src/Document.php
@@ -19,6 +19,12 @@ class Document implements Buildable, Contentable
     use BuildableImp;
     use ContentableImp;
 
+    private string $version = '1.0';
+
+    private string $encoding = 'UTF-8';
+
+    private string $standalone = 'yes';
+
     final private function __construct(
         private string $name,
         string|Stringable ...$content
@@ -26,10 +32,31 @@ class Document implements Buildable, Contentable
         $this->content  = $content;
     }
 
+    public function setVersion(string|float|int $version): self
+    {
+        if (is_int($version)) {
+            $version = strval($version) . '.0';
+        }
+        $this->version = strval($version);
+        return $this;
+    }
+
+    public function setEncoding(string $encoding): self
+    {
+        $this->encoding = $encoding;
+        return $this;
+    }
+
+    public function setStandalone(bool $standalone = true): self
+    {
+        $this->standalone = ($standalone) ? 'yes' : 'no';
+        return $this;
+    }
+
     public function __toString(): string
     {
         $doctype =
-            '<?xml version = "1.0" encoding = "UTF-8" standalone = "yes" ?>'
+            '<?xml version = "' . $this->version . '" encoding = "' . $this->encoding . '" standalone = "' . $this->standalone . '" ?>'
             . "\n";
         return $doctype . $this->opening() . implode('', $this->content)
             . $this->closing();
--- tests/PerformanceTest.php
+++ tests/PerformanceTest.php
@@ -62,7 +62,7 @@ class PerformanceTest extends TestCase
         $elapsed = $end - $start;
         $ms      = $elapsed/1e+6;
 
-        $this->assertLessThan(0.1, $ms);
+        $this->assertLessThan(0.2, $ms);
     }
 
     /**
